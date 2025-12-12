6edb99ee2dd7f4910d3ebaac606b0d8b2993c8ad
ScribeE-coder <kidmusician006@gmail.com>
2025-12-11 21:36:47 -0800

    Currently working on an Among Us Clone collision detection is working fine

---
+++ amongUsClone.py
@@ -0,0 +1,147 @@
+import pygame, math 
+
+SCREEN_WIDTH = 640 
+SCREEN_HEIGHT = 640 
+
+pygame.init()
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Among Us")
+clock = pygame.time.Clock()
+
+cafeteria = pygame.image.load("amongUs_caf.PNG") 
+cafeteria = pygame.transform.scale(cafeteria, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+yellow_crew = pygame.image.load("amongus_yellow_char-Picsart-BackgroundRemover.jpeg").convert_alpha()
+yellow_crew = pygame.transform.scale(yellow_crew, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
+
+crew_sprite_sheet = None 
+
+obstacles = [] 
+
+centers = {"upper_right": (443, 180), 
+           "emergency": (312, 332),
+           "upper_left": (173, 182), 
+           "bottom_right": (444, 474),
+           "bottom_left": (175, 473)
+           }
+
+class Obstacle(): 
+    def __init__(self, x, y, radius): 
+        self.x = x 
+        self.y = y
+        self.radius = radius 
+        self.center_x = self.x 
+        self.center_y = self.y       
+
+""" the next step is to work on crewmate movement animation"""
+class CrewMate(): 
+    def __init__(self, crew_img, x, y, width, height, speed=2, killed=False): 
+        self.crew = crew_img 
+        self.x = x 
+        self.y = y 
+        self.width = width 
+        self.height = height 
+        self.speed = speed
+        self.killed = killed 
+        self.crew_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        #crewmate collision properties 
+        self.radius = self.width / 2 
+        self.center_x = self.x + self.width / 2 
+        self.center_y = self.y + self.height / 2 
+    
+    # check for whether crewmate is colliding with other objects 
+    def collision_table_check(self, obstacles):
+        
+        for object in obstacles: 
+            # calculating distance between centers 
+            distance = math.sqrt((self.center_x - object.center_x ) ** 2 + (self.center_y - object.center_y) ** 2) 
+
+            # collision detected 
+            if distance < (self.radius + object.radius): 
+                return True      
+    
+    def crew_move(self, keys): 
+        old_x = self.x  
+        old_y = self.y 
+
+        if keys[pygame.K_w]:
+            # move upwards 
+            self.y -= self.speed
+       
+        if keys[pygame.K_a]: 
+            # move to the left 
+            self.x -= self.speed 
+        
+        if keys[pygame.K_s]: 
+            #move downwards 
+            self.y += self.speed
+        
+        if keys[pygame.K_d]:  
+            #move to the right
+            self.x += self.speed
+
+        # update center after movement 
+        self.center_x = self.x + self.width / 2 
+        self.center_y = self.y + self.height / 2 
+
+        # collision check after movement 
+        if self.collision_table_check(obstacles): 
+            self.x = old_x
+            self.y = old_y 
+
+            # update center after moving backwards 
+            self.center_x = self.x + self.width / 2 
+            self.center_y = self.y + self.height / 2
+        
+    def crew_draw(self): 
+        window.blit(self.crew, (self.x, self.y))
+
+# will work on later 
+class Impostor(): 
+    def __init__(self, imp_img, x, y, width, height, speed=2): 
+        self.imp_img = imp_img 
+        self.x = x 
+        self.y = y 
+        self.width = width 
+        self.height = height
+
+    def imp_move(self): 
+        return None 
+    
+    def kill(self): 
+        return None 
+
+table_radius = 50
+    
+yellow_crew = CrewMate(yellow_crew, 320, 380, SCREEN_WIDTH/17, SCREEN_WIDTH/17) 
+
+upper_right_table = Obstacle(centers.get("upper_right")[0], centers.get("upper_right")[1], table_radius)
+emergency_table = Obstacle(centers.get("emergency")[0], centers.get("emergency")[1], table_radius) 
+upper_left_table = Obstacle(centers.get("upper_left")[0], centers.get("upper_left")[1], table_radius)
+bottom_right_table = Obstacle(centers.get("bottom_right")[0], centers.get("bottom_right")[1], table_radius) 
+bottom_left_table = Obstacle(centers.get("bottom_left")[0], centers.get("bottom_left")[1], table_radius) 
+
+tables = [upper_right_table, emergency_table, upper_left_table, bottom_right_table, bottom_left_table] 
+for table in tables: 
+    obstacles.append(table)
+ 
+ # put your images on your created display    
+def draw(): 
+    window.blit(cafeteria, (0, 0))
+    yellow_crew.crew_draw() 
+
+running = True 
+
+while running: 
+    # keys = list of all possible keys that can be pressed on the keyboard 
+    keys = pygame.key.get_pressed()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            running = False
+
+    yellow_crew.crew_move(keys) 
+    draw()
+    pygame.display.update()
+    clock.tick(60) 
