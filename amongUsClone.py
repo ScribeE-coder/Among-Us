54058c9d1aaf7d85af14e4a39f34728f15c48d40
ScribeE-coder <kidmusician006@gmail.com>
2025-12-22 14:44:15 -0800

    currently working on collision detection for other objects

--- amongUsClone.py
+++ amongUsClone.py
@@ -105,6 +105,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos() 
+            print(pos)
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_t:
