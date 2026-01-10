72e476784f1f65f7b81e5e41b2f011111b66feae
ScribeE-coder <kidmusician006@gmail.com>
2026-01-10 12:35:49 -0800

    finished cleaning up main file

--- amongUsClone.py
+++ amongUsClone.py
@@ -8,124 +8,78 @@ from Impostor import Monster
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640 
+divisor = 15 
 
 pygame.init()
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Among Us Clone")
 clock = pygame.time.Clock()
 
-def load_image(img_file): 
-    file = pygame.image.load(img_file)
-    return file 
+# function makes file clean and easier to read 
+def load_img(file_path, scale_width, scale_height): 
+    img = pygame.image.load(file_path)
+    img = pygame.transform.scale(img, ((scale_width, scale_height)))
+    return img 
 
-def scale(img_file, scale_width, scale_height): 
-    file = pygame.transform.scale(img_file, )(scale_width, scale_height)
-    return file 
+cafeteria = load_img("images/amongUS_caf.PNG", SCREEN_WIDTH, SCREEN_HEIGHT) 
 
-cafeteria = pygame.image.load("images/amongUs_caf.PNG") 
-cafeteria = pygame.transform.scale(cafeteria, (SCREEN_WIDTH, SCREEN_HEIGHT))
+yellow_crew = load_img("images/amongUs_yellowCrew.jpeg", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+idle_crew = load_img("images/idle.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor) 
 
-yellow_crew = pygame.image.load("images/amongUs_yellowCrew.jpeg")
-yellow_crew = pygame.transform.scale(yellow_crew, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
+crew_walk1 = load_img("images/crewWalk1.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk2 = load_img("images/crewWalk2.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk3 = load_img("images/crewWalk3.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk4 = load_img("images/crewWalk4.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk5 = load_img("images/crewWalk5.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk6 = load_img("images/crewWalk6.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+crew_walk7 = load_img("images/crewWalk7.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
 
-idle_crew = pygame.image.load("images/idle.png")
-idle_crew = pygame.transform.scale(idle_crew, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-
-crew_walk1 = pygame.image.load("images/crewWalk1.png") 
-crew_walk2 = pygame.image.load("images/crewWalk2.png")
-crew_walk3 = pygame.image.load("images/crewWalk3.png") 
-crew_walk4 = pygame.image.load("images/crewWalk4.png") 
-crew_walk5 = pygame.image.load("images/crewWalk5.png") 
-crew_walk6 = pygame.image.load("images/crewWalk6.png") 
-crew_walk7 = pygame.image.load("images/crewWalk7.png")
-
-# squishing down images to scale 
-crew_walk1 = pygame.transform.scale(crew_walk1, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-crew_walk2 = pygame.transform.scale(crew_walk2, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15)) 
-crew_walk3 = pygame.transform.scale(crew_walk3, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15)) 
-crew_walk4 = pygame.transform.scale(crew_walk4, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-crew_walk5 = pygame.transform.scale(crew_walk5, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-crew_walk6 = pygame.transform.scale(crew_walk6, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-crew_walk7 = pygame.transform.scale(crew_walk7, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15)) 
 
 crew_walking_right = [crew_walk1, crew_walk2, crew_walk3, crew_walk4, crew_walk5, crew_walk6, crew_walk7] 
 crew_walking_left = [pygame.transform.flip(sprite, True, False) for sprite in crew_walking_right]
 
-monsterTransform1 = pygame.image.load("images/impTransform1.png")
-monsterTransform2 = pygame.image.load("images/impTransform2.png")
-monsterTransform3 = pygame.image.load("images/impTransform3.png")
-monsterTransform4 = pygame.image.load("images/impTransform4.png")
-monsterTransform5 = pygame.image.load("images/impTransform5.png")
-monsterTransform6 = pygame.image.load("images/impTransform6.png")
-monsterTransform7 = pygame.image.load("images/impTransform7.png")
-monsterTransform8 = pygame.image.load("images/impTransform8.png")
-monsterTransform9 = pygame.image.load("images/impTransform9.png")
-monsterTransform10 = pygame.image.load("images/impTransform10.png")
-monsterTransform11 = pygame.image.load("images/impTransform11.png")
-monsterTransform12 = pygame.image.load("images/impTransform12.png")
-monsterTransform13 = pygame.image.load("images/impTransform13.png")
-
-monsterTransform1 = pygame.transform.scale(monsterTransform1, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform2 = pygame.transform.scale(monsterTransform2, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform3 = pygame.transform.scale(monsterTransform3, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform4 = pygame.transform.scale(monsterTransform4, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform5 = pygame.transform.scale(monsterTransform5, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform6 = pygame.transform.scale(monsterTransform6, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform7 = pygame.transform.scale(monsterTransform7, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform8 = pygame.transform.scale(monsterTransform8, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform9 = pygame.transform.scale(monsterTransform9, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform10 = pygame.transform.scale(monsterTransform10, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform11 = pygame.transform.scale(monsterTransform11, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform12 = pygame.transform.scale(monsterTransform12, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterTransform13 = pygame.transform.scale(monsterTransform13, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
+monsterTransform1 = load_img("images/impTransform1.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform2 = load_img("images/impTransform2.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform3 = load_img("images/impTransform3.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform4 = load_img("images/impTransform4.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform5 = load_img("images/impTransform5.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform6 = load_img("images/impTransform6.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform7 = load_img("images/impTransform7.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform8 = load_img("images/impTransform8.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform9 = load_img("images/impTransform9.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform10 = load_img("images/impTransform10.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform11 = load_img("images/impTransform11.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform12 = load_img("images/impTransform12.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterTransform13 = load_img("images/impTransform13.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
 
 monster_transform_list = [monsterTransform1, monsterTransform2, monsterTransform3, monsterTransform4, monsterTransform5, monsterTransform6, monsterTransform7, monsterTransform8, monsterTransform9, monsterTransform10, monsterTransform11, monsterTransform12, monsterTransform13]
 
-monsterWalk1 = pygame.image.load("images/monsterWalk1.png")
-monsterWalk2 = pygame.image.load("images/monsterWalk2.png")
-monsterWalk3 = pygame.image.load("images/monsterWalk3.png")
-monsterWalk4 = pygame.image.load("images/monsterWalk4.png")
-monsterWalk5 = pygame.image.load("images/monsterWalk5.png")
-monsterWalk6 = pygame.image.load("images/monsterWalk6.png")
-monsterWalk7 = pygame.image.load("images/monsterWalk7.png")
+monsterWalk1 = load_img("images/monsterWalk1.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterWalk2 = load_img("images/monsterWalk2.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterWalk3 = load_img("images/monsterWalk3.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterWalk4 = load_img("images/monsterWalk4.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterWalk5 = load_img("images/monsterWalk5.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor) 
+monsterWalk6 = load_img("images/monsterWalk6.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor) 
+monsterWalk7 = load_img("images/monsterWalk7.png", SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
 
-monsterWalk1 = pygame.transform.scale(monsterWalk1, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk2 = pygame.transform.scale(monsterWalk2, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk3 = pygame.transform.scale(monsterWalk3, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk4 = pygame.transform.scale(monsterWalk4, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk5 = pygame.transform.scale(monsterWalk5, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk6 = pygame.transform.scale(monsterWalk6, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterWalk7 = pygame.transform.scale(monsterWalk7, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15)) 
 
 monster_walk_right = [monsterWalk1, monsterWalk2, monsterWalk3, monsterWalk4, monsterWalk5, monsterWalk6, monsterWalk7]
 monster_walk_left = [pygame.transform.flip(sprite, True, False) for sprite in monster_walk_right] 
 
 # animation cycle for monster attacking 
-monsterAttack1 = pygame.image.load('images/monsterAttack1.png')
-monsterAttack2 = pygame.image.load('images/monsterAttack2.png')
-monsterAttack3 = pygame.image.load('images/monsterAttack3.png')
-monsterAttack4 = pygame.image.load('images/monsterAttack4.png')
-monsterAttack5 = pygame.image.load('images/monsterAttack5.png')
-monsterAttack6 = pygame.image.load('images/monsterAttack6.png')
-monsterAttack7 = pygame.image.load('images/monsterAttack7.png')
-monsterAttack8 = pygame.image.load('images/monsterAttack8.png')
-monsterAttack9 = pygame.image.load('images/monsterAttack9.png')
-monsterAttack10 = pygame.image.load('images/monsterAttack10.png')
-monsterAttack11 = pygame.image.load('images/monsterAttack11.png')
-monsterAttack12 = pygame.image.load('images/monsterAttack12.png')
 
-monsterAttack1 = pygame.transform.scale(monsterAttack1, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack2 = pygame.transform.scale(monsterAttack2, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack3 = pygame.transform.scale(monsterAttack3, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack4 = pygame.transform.scale(monsterAttack4, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack5 = pygame.transform.scale(monsterAttack5, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack6 = pygame.transform.scale(monsterAttack6, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack7 = pygame.transform.scale(monsterAttack7, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack8 = pygame.transform.scale(monsterAttack8, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack9 = pygame.transform.scale(monsterAttack9, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack10 = pygame.transform.scale(monsterAttack10, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack11 = pygame.transform.scale(monsterAttack11, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
-monsterAttack12 = pygame.transform.scale(monsterAttack12, (SCREEN_WIDTH/15, SCREEN_HEIGHT/15))
+monsterAttack1 = load_img('images/monsterAttack1.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack2 = load_img('images/monsterAttack2.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack3 = load_img('images/monsterAttack3.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack4 = load_img('images/monsterAttack4.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack5 = load_img('images/monsterAttack5.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack6 = load_img('images/monsterAttack6.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack7 = load_img('images/monsterAttack7.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack8 = load_img('images/monsterAttack8.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack9 = load_img('images/monsterAttack9.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack10 = load_img('images/monsterAttack10.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack11 = load_img('images/monsterAttack11.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
+monsterAttack12 = load_img('images/monsterAttack12.png', SCREEN_WIDTH/divisor, SCREEN_HEIGHT/divisor)
 
 monster_attack_cycle = [monsterAttack1, monsterAttack2, monsterAttack3, monsterAttack4, monsterAttack5, monsterAttack6, monsterAttack7, monsterAttack8, monsterAttack9, monsterAttack10, monsterAttack11, monsterAttack12]
 
